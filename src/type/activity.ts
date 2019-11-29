00d6e76999137de6b285cbd51f8610c5752f8662
Ted Shaffer <ted@brightsign.biz>
2019-11-29 06:27:22 -0800

    corrections

--- src/type/activity.ts
+++ src/type/activity.ts
@@ -1,18 +1,72 @@
-import { StravaNativeSummarySegmentEffort, StravaNativeSummaryAthlete, StravaNativeLatLng, StravaNativePolylineMap, StravaNativeDetailedSegmentEffort } from "./base";
+import { StravaNativeSummarySegmentEffort, StravaNativeSummaryAthlete, StravaNativeLatLng, StravaNativePolylineMap, StravaNativeDetailedSegmentEffort, StravaNativeMetaAthlete } from "./base";
 
 export interface StravaNativeMetaActivity {
   id: number;
   resource_state: number;
 }
-
+/*
+{ resource_state: 2,
+  athlete: { id: 2843574, resource_state: 1 },
+  name: 'Wilder with Lori',
+  distance: 17740.9,
+  moving_time: 5758,
+  elapsed_time: 8479,
+  total_elevation_gain: 423,
+  type: 'Ride',
+  workout_type: 10,
+  id: 2836726906,
+  external_id: 'garmin_push_4216601163',
+  upload_id: 3006890783,
+  start_date: '2019-11-02T16:58:21Z',
+  start_date_local: '2019-11-02T09:58:21Z',
+  timezone: '(GMT-08:00) America/Los_Angeles',
+  utc_offset: -25200,
+  start_latlng: [ 36.96, -122.09 ],
+  end_latlng: [ 36.96, -122.09 ],
+  location_city: null,
+  location_state: null,
+  location_country: 'United States',
+  start_latitude: 36.96,
+  start_longitude: -122.09,
+  achievement_count: 4,
+  kudos_count: 1,
+  comment_count: 0,
+  athlete_count: 2,
+  photo_count: 0,
+  map: 
+   { id: 'a2836726906',
+     summary_polyline: '{wq`F|xchVOU_AW|@iI@iA_@g@kBRwB`EyA~@iBh@cDZiAAmCaA_D_BuB[_AeAiDPgCWIEDOmB_@qBT{Ai@iCNcC~@qCXaAp@aBg@kBQKSBw@mAw@cBaBeAoCCuE_@g@mAm@iAO}FGi@KgAs@D|@v@zBq@l@kAEGd@VfAm@lA_CJxCbFDXUz@gBXq@s@qAK{@Za@a@yA]O_@e@yDs@jEmA~@sBl@~A{AF_@IoBQk@w@oB}AcCa@SqAAq@QWNeAbCSz@Lx@hAjBV`AXpCIl@qAUgBRqDrAiAAcFiBc@_C]uQLaFNaALWhBFtCK|EbBbAp@nAvCx@d@K`BSLcA|BUlANr@fAfBVlAR~BQt@w@OoBHcAj@{Cp@}Cs@uAk@c@`@s@CyAgAWq@w@e@CpBGr@Un@Ej@q@jAc@^{CCwAx@]BjBZPNX|@dCfAfBVJn@cAtAkATsAM_CmA}AEuFt@u@f@mGh@e@\\Yp@hAbBhCi@\\@d@h@j@L|Ac@~@ChG`DTn@SrBf@fA_@rA}@\\mAo@qBN`BbCXPx@NhD|AbAC@R{@vBcAvGsAdD}@fD?L`ATm@hCsAfDiAfF_@hEQp@HlCkAh@}AlDeAjA_@dCK~CSV}@Dw@lAqANa@f@[FQd@Jf@vA_@rBx@ZAb@_@^TFNC~@Lf@`ApBGp@q@[GDBd@Rr@b@`@|@KSnBV~BbCdGDVGVPSoCmGWwB`@oAwByA[u@@Qn@h@R@Hi@aAcDGa@Dq@[e@OAW\\YA{Bu@a@H]^i@GJkAvCoA`@eAtAUP_B?gA`@gCfAoAfAoCl@k@l@[z@vBM`BFzD`BzCPHv@gBjAmENaCs@mCG{@`@mEN_@zB}Bn@STWcCy@M_Ae@[p@yCbAeAJaAd@yAPiAt@g@tADf@]rCUJm@`CkD\\u@l@oC\\mDjAiAzBs@hCXrAj@~C?`@ElCsBfCyEEiA\\}@NaBOgCFc@d@o@d@qAf@a@tEiApBPpB{ATAv@r@h@ClIwCvAsAZGfC`@jBx@|ATf@n@`AvCRXl@TrAChCiBfAQzClAbFl@zItBvH@|@[rB_Cf@cA?m@c@mBo@gAeD_CoAc@bBBdDrA~FGhBq@~AaAfA}Ah@uArB]V\\C~Ay@tGRv@z@PMN',
+     resource_state: 2 },
+  trainer: false,
+  commute: false,
+  manual: false,
+  private: false,
+  visibility: 'everyone',
+  flagged: false,
+  gear_id: 'b1803050',
+  from_accepted_tag: false,
+  upload_id_str: '3006890783',
+  average_speed: 3.081,
+  max_speed: 9.7,
+  average_temp: 16,
+  average_watts: 97.5,
+  kilojoules: 561.4,
+  device_watts: false,
+  has_heartrate: false,
+  heartrate_opt_out: false,
+  display_hide_heartrate_option: false,
+  elev_high: 194.4,
+  elev_low: 3.8,
+  pr_count: 1,
+  total_photo_count: 0,
+  has_kudoed: false }
+*/
 // https://developers.strava.com/docs/reference/#api-models-SummaryActivity
 // TEDTODO - some of the properties here may in fact only be in the detailed activity
 export interface StravaNativeSummaryActivity {
   achievement_count: number;
-  athlete: any; // MetaAthlete??, but really { id, resource_state }
+  athlete: StravaNativeMetaAthlete;
   athlete_count: number;
-  average_cadence: number;
-  average_heartrate: number;
   average_speed: number;
   average_temp: number;
   average_watts: number;
@@ -41,13 +95,9 @@ export interface StravaNativeSummaryActivity {
   manual: boolean;
   map: StravaNativePolylineMap;
   max_speed: number;
-  max_heartrate: number;
-  max_watts: number;
   moving_time: number;
   name: string;
-  perceived_exertion: any;
   photo_count: number;
-  prefer_perceived_exertion: any;
   pr_count: number;
   private: boolean;
   resource_state: number;
@@ -66,7 +116,6 @@ export interface StravaNativeSummaryActivity {
   utc_offset: number;
   visibility: string;
   workout_type: number;
-  weighted_average_watts: number;
 }
 
 // https://developers.strava.com/docs/reference/#api-models-DetailedActivity
@@ -75,13 +124,20 @@ export interface StravaNativeDetailedActivity extends StravaNativeSummaryActivit
   photos: any; // PhotosSummary
   gear: string; // SummaryGear
   calories: number;
-  segment_efforts: StravaNativeDetailedSegmentEffort[]; // DetailedSegmentEffort
+  segment_efforts: StravaNativeDetailedSegmentEffort[];
   device_name: string;
   embed_token: string;
   splits_metric: any; // Split
   splits_standard: any; // Split
   laps: any; // Lap
   best_efforts: any; // DetailedSegmentEffort
+  average_cadence: number;
+  average_heartrate: number;
+  max_heartrate: number;
+  max_watts: number;
+  perceived_exertion: any;
+  prefer_perceived_exertion: any;
+  weighted_average_watts: number;
 }
 
 export interface StravatronSummaryActivity {
